c3986e4cbde342bd328b4fe2444d8b0953bc9350
Sai Aung Moe Oo <saiaungmoeoo.lk@gmail.com>
2024-02-28 14:50:54 +0630

    fix bugs

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
+import express from "express";
+const app = express();
+import http from "http";
 import dotenv from "dotenv";
 import TelegramBot from "node-telegram-bot-api";
 import { get2d, get3d, getJoke, getNews, getQuote } from "./api.js";
 dotenv.config();
 
 const TELEGRAM_BOT_API_KEY = process.env.TELEGRAM_BOT_API_KEY;
+
+//ENV
+const PORT = process.env.PORT || 8000;
+const server = http.createServer(app);
 
 const telegramBot = new TelegramBot(TELEGRAM_BOT_API_KEY, { polling: true });
 
@@ -39,4 +46,8 @@ telegramBot.onText(/\/news_(\w+)/, async (msg, match) => {
   } else {
     telegramBot.sendMessage(msg.chat.id, `There is no news about ${search}.`);
   }
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
